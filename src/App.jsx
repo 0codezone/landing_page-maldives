211fbb1b9e9bb38371448ee8cca163200011da60
0codezone <mohittailor997@gmail.com>
2024-02-29 19:29:17 +0530

    add offer and TripPlan section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Offer from "./components/Offer";
+import PlanTrip from "./components/PlanTrip";
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Offer />
+      <PlanTrip />
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,8 @@ const Navbar = () => {
   const [navOpen, setNavOpen] = useState(false);
   if (navOpen) {
     document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "auto";
   }
 
   return (

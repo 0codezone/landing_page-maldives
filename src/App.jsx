bcc49fc5a672be7af968e1f2dae85c743f33a649
0codezone <mohittailor997@gmail.com>
2024-02-29 21:38:34 +0530

    add room section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Offer from "./components/Offer";
 import PlanTrip from "./components/PlanTrip";
+import Rooms from "./components/Rooms";
 const App = () => {
   return (
     <div>
@@ -9,6 +10,7 @@ const App = () => {
       <Hero />
       <Offer />
       <PlanTrip />
+      <Rooms />
     </div>
   );
 };
